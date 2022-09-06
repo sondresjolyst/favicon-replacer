f61ed24e5f806f525d7b1d5903c8bc0625979dde
sondresjolyst <sonyslyst@gmail.com>
2022-09-06 17:42:33 +0200

    resize image script

---
+++ resizer.py
@@ -0,0 +1,19 @@
+import os
+from PIL import Image
+
+inputDir = 'images'
+outputDir = 'output'
+original = Image.open('original.png')
+
+print("starting resizer!")
+
+for imageName in os.listdir(inputDir):
+    f = os.path.join(inputDir, imageName)
+    image = Image.open(f)
+    size = image.size
+
+    new_image = original.resize(size)
+    new_image.save(os.path.join(outputDir, imageName))
+    print(f'{os.path.join(outputDir, imageName)}')
+
+print("Done!")
